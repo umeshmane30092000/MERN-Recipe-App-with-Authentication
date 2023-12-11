905a3fa553a73932877fd97bc9bb5115ca106ffd
Umesh <umeshmane575@gmail.com>
2023-12-11 07:12:12 +0530

    online db is addded

--- server/src/index.js
+++ server/src/index.js
@@ -10,11 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// mongodb://127.0.0.1:27017/recipeapp
+
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
 
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/recipeapp")
+mongoose.connect("mongodb+srv://umeshmane20:Mane%403009@cluster0.ercg4oc.mongodb.net/recipeapp?retryWrites=true&w=majority")
   .then(() => {
     console.log('Connected to MongoDB');
   })
